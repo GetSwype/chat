0e1fdc744c8b72fd69ead0d97b6160fdf8e4fa4b
srikanthsrnvs <srikanth.srinivas@mail.utoronto.ca>
2023-03-01 11:21:35 -0800

    model

--- server.ts
+++ server.ts
@@ -6,7 +6,16 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
+
+app.post('/chat', (req: Request, res: Response) => {
+    const {  } = req.body;
+});
+
+app.post('/register', (req: Request, res: Response) => {
+    const { phone_number } = req.body;
+    console.info("Registering user with phone number: " + phone_number);
+
+    
     res.send('Express + TypeScript Server');
 });
 
