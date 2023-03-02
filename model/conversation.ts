7ec04dbfd25781c5dd83097637135395d73622e3
srikanthsrnvs <srikanth.srinivas@mail.utoronto.ca>
2023-03-02 11:42:24 -0800

    Fix to sharing contacts

--- model/conversation.ts
+++ model/conversation.ts
@@ -2,7 +2,6 @@ import { PrismaClient, Conversation } from "@prisma/client"
 import Storage from "./storage"
 import { OpenAI } from "./openai";
 import { IMessage } from "./imessage";
-import { get_encoding, encoding_for_model } from "@dqbd/tiktoken";
 
 
 function ConversationExtension(prisma_conversation: PrismaClient['conversation'] = Storage.instance.conversation) {
@@ -17,17 +16,38 @@ function ConversationExtension(prisma_conversation: PrismaClient['conversation']
                 }
             });
             
-            await OpenAI.getInstance().complete(messages, phone_number);
-            const latest_message = await Storage.instance.message.findFirst({
-                where: {
-                    conversation_id: conversation_id
-                },
-                orderBy: {
-                    created_at: "desc"
+            if (messages.length > 10) {
+                await Storage.instance.message.create({
+                    data: {
+                        text: "You'll need to share a contact with me to continue chatting! Head into your phone's contacts and share a contact with me who'd like to try out ChatGPT :) This helps keep the service free, and grow to new users",
+                        author: "assistant",
+                        conversation_id: conversation_id
+                    }
+                })
+                let message = await Storage.instance.message.findFirst({
+                    where: {
+                        conversation_id: conversation_id
+                    },
+                    orderBy: {
+                        created_at: "desc"
+                    }
+                })
+                await IMessage.getInstance().send(message, phone_number);
+                return
+            }else {
+                await OpenAI.getInstance().complete(messages, phone_number);
+                const latest_message = await Storage.instance.message.findFirst({
+                    where: {
+                        conversation_id: conversation_id
+                    },
+                    orderBy: {
+                        created_at: "desc"
+                    }
+                });
+                if (latest_message) {
+                    await IMessage.getInstance().send(latest_message, phone_number)
                 }
-            });
-            if (latest_message) {
-                await IMessage.getInstance().send(latest_message, phone_number)
+                return
             }
         }
     });
